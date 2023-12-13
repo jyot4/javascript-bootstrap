426cfdad6ad63767b2a0e7d31fd031f7058b83d8
jyot4 <jyotivyass2000@gmail.com>
2023-12-13 11:35:35 +0530

    transfering all to github

--- circle/main.js
+++ circle/main.js
@@ -1,5 +1,8 @@
 const first = document.querySelector(".first")
 
+const randomCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const colorText ="abcdef1234567890"
+
 first.onclick= (e)=>{
 
 const mycircle = document.createElement('div')
@@ -7,8 +10,24 @@ const mycircle = document.createElement('div')
 mycircle.setAttribute('class' , 'circle')
 first.append(mycircle)
 
-mycircle.style.left  = e.clientX + 'px'
-mycircle.style.top  = e.clientY + 'px'
+mycircle.style.left  = e.clientX -'13' + 'px'
+mycircle.style.top  = e.clientY -'13' + 'px'
 
-    // console.log('hello')
+
+
+let hash = "#"
+let colorSplitted = colorText.split("")
+// let colorSplitted = colorText.charAt('')
+
+mycircle.style.backgroundColor = `${hash}${colorSplitted[Math.floor(Math.random()*colorText.length)]}${colorSplitted[Math.floor(Math.random()*colorText.length)]}${colorSplitted[Math.floor(Math.random()*colorText.length)]}`
+
+
+
+
+// mycircle.style.backgroundColor = hash + color
+const splitted = randomCharacter.split("")
+mycircle.textContent = splitted[Math.floor(Math.random()*randomCharacter.length)]
+    // console.log(mycircle.textContent)
 }
+
+
